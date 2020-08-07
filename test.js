b73eadfb354aa63623bb4c571a918ad9d8d95690
Prathik Ramakrishnan <pramakrishnan@twitter.com>
2020-08-07 18:32:54 +0530

    Connected to testrail

---
+++ test.js
@@ -0,0 +1,23 @@
+const get  = require('request')
+var auth = Buffer.from('alcatrazwatchesnetflix@gmail.com' + ":" + "Natflix.525").toString('base64')   
+
+const options = {
+    method: "GET",
+    url: "https://pratlegacy.testrail.io/index.php?/api/v2/get_runs/1",
+    headers: {
+        "Authorization": "Basic " + auth,
+        "Content-Type": "application/json"
+    }
+}
+
+get(options, (error, response, body) => {
+    if((JSON.parse(response["body"])["error"])){
+        console.log((JSON.parse(response["body"])["error"] +'-> Could not connect to Testrail '))
+    }
+    else{
+        console.log("Successfully connected to Testrail!")
+        var site = JSON.parse(body);
+        console.log(site);
+   }})
+
+
